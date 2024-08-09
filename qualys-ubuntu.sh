3488939c0f8fc144d6123b55ec270c0f6ef1e295
RafaelMorales <nanaya2926@gmail.com>
2024-08-08 18:50:06 -0700

    Primera version funcional del script

---
+++ qualys-ubuntu.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Variables
+DOWNLOAD_URL="https://data.rafalan.com/web/client/pubshares/epwySsnqsKnPE9hkn98JXb?compress=false"
+FILE_NAME="QualysCloudAgent.deb"
+USER="ramoralesl"
+PASSWORD="123"
+
+# Función para descargar el archivo desde el nuevo enlace
+download_file() {
+    if [ -f "${FILE_NAME}" ]; then
+        echo "El archivo ${FILE_NAME} ya existe en el directorio. No es necesario descargarlo nuevamente."
+    else
+        echo "Descargando archivo desde el nuevo enlace..."
+        wget "${DOWNLOAD_URL}" -O ${FILE_NAME}
+        
+        if [ $? -ne 0 ]; then
+            echo "Error al descargar el archivo."
+            exit 1
+        fi
+        
+        echo "Archivo descargado como ${FILE_NAME}"
+    fi
+}
+
+# Función para instalar el paquete .deb
+install_deb_package() {
+    echo "Verificando si el servicio 'qualys-cloud-agent' está activo..."
+    
+    # Verificar el estado del servicio
+    if systemctl status qualys-cloud-agent.service >/dev/null 2>&1; then
+        echo "El servicio 'qualys-cloud-agent' ya está activo."
+    else
+        echo "Instalando ${FILE_NAME}..."
+        
+        sudo dpkg -i ${FILE_NAME}
+        
+        if [ $? -ne 0 ]; then
+            echo "Error al instalar el paquete ${FILE_NAME}. Intentando reparar dependencias..."
+            sudo apt-get install -f -y
+        fi
+        
+        echo "Instalación de ${FILE_NAME} completada."
+    fi
+}
+
+
+# Función para activar el agente de Qualys
+activate_qualys_agent() {
+    echo "Activando el agente de Qualys..."
+    
+    sudo /usr/local/qualys/cloud-agent/bin/qualys-cloud-agent.sh 	ActivationId=e1f4f34f-8e56-4f2e-8336-484624b77091 CustomerId=72528eda-c400-5ba6-81d6-a154df946c4b ServerUri=https://qagpublic.qg4.apps.qualys.com/CloudAgent/
+
+    
+    if [ $? -ne 0 ]; then
+        echo "Error al activar el agente de Qualys."
+        exit 1
+    fi
+    
+    echo "Agente de Qualys activado."
+}
+
+# Función principal
+main() {
+    download_file
+    
+    # Ejecutar las funciones como usuario 'soporte'
+    echo "${PASSWORD}" | sudo -S -u ${USER} bash -c "$(declare -f install_deb_package); install_deb_package; $(declare -f activate_qualys_agent); activate_qualys_agent"
+    
+    echo "Script completado."
+}
+
+# Ejecutar la función principal
+main
